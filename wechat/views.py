20c0cf9afad0f64c4b124796edb8498b4353bd5a
Saber <642964056@qq.com>
2018-09-12 16:28:11 +0800

    "fix token"

--- wechat/views.py
+++ wechat/views.py
@@ -16,26 +16,33 @@ from django.http import HttpResponse
 def weixin_main(request):
     if request.method == "GET":
         # 接收微信服务器get请求发过来的参数
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('nonce', None)
-        # 服务器配置中的token = 'hello'
-        token = 'hello123'
-        # 把参数放到list中排序后合成一个字符串，
-        # 再用sha1加密得到新的字符串与微信发来的signature对比，
-        # 如果相同就返回echostr给服务器，校验通过
-        hashlist = [token, timestamp, nonce]
-        hashlist.sort()
-        hashstr = ''.join([s for s in hashlist])
-        hashstr = hashlib.sha1(hashstr).hexdigest()
-        if hashstr == signature:
-            return HttpResponse(echostr)
-        else:
-            return HttpResponse("field")
+        try:
+            signature = request.GET.get('signature', None)
+            timestamp = request.GET.get('timestamp', None)
+            nonce = request.GET.get('nonce', None)
+            echostr = request.GET.get('echostr', None)
+            # 服务器配置中的token = 'hello'
+            token = 'hello'
+            # 把参数放到list中排序后合成一个字符串，
+            # 再用sha1加密得到新的字符串与微信发来的signature对比，
+            # 如果相同就返回echostr给服务器，校验通过
+            hashlist = [token, timestamp, nonce]
+            hashlist.sort()
+            hashstr = ''.join([s for s in hashlist])
+            # hashstr = hashlib.sha1(hashstr).hexdigest()
+            hashstr = hashlib.sha1()
+            map(hashstr.update, hashlist)
+            hashcode = hashstr.hexdigest()
+            if hashstr == signature:
+                return HttpResponse(echostr)
+            else:
+                return HttpResponse("field")
+        except:
+            return HttpResponse('helloworld')
     else:
         othercontent = autoreply(request)
-        return HttpResponse(othercontent)
+        # return HttpResponse(othercontent)
+
 
 
 #微信服务器推送消息是xml的，根据利用ElementTree来解析出的不同xml内容返回不同的回复信息，
